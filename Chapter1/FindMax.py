90d8cbf225ae5706dc97ed4da9b68a8a911d16ad
Geonhee Cho <36052735+gunny91@users.noreply.github.com>
2021-03-14 12:15:30 +0900

    Phyton basic datastructure

---
+++ Chapter1/FindMax.py
@@ -0,0 +1,11 @@
+print('Find Max value from 3 integers')
+
+a = int(input('Type integer a')) 
+b = int(input('Type integer b'))
+c = int(input('Type integer c'))
+
+max = a                 #Set the temparary max value
+if b > max: max = b.    # if b value is greater than max, then max =b
+if c > max: max = c.  
+
+print(f'Max is {max}')  #return the max value

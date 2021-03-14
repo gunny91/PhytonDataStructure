4c988734bcc91db2de734cea24f30958c1175230
gunny <gunny910303@gmail.com>
2021-03-14 20:43:10 +0900

    Change the code

--- Chapter1/FindMax.py
+++ Chapter1/FindMax.py
@@ -4,8 +4,8 @@ a = int(input('Type integer a'))
 b = int(input('Type integer b'))
 c = int(input('Type integer c'))
 
-max = a                 #Set the temparary max value
-if b > max: max = b.    # if b value is greater than max, then max =b
-if c > max: max = c.  
+max = a                 #Set the temporary max value
+if b > max: max = b    # if b value is greater than max, then max =b
+if c > max: max = c
 
 print(f'Max is {max}')  #return the max value

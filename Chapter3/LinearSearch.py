c4f29fed9e613754df7c72f372edb8889f6504df
gunny <gunny910303@gmail.com>
2021-03-31 23:09:25 +0900

    Search statement update

--- Chapter3/LinearSearch.py
+++ Chapter3/LinearSearch.py
@@ -1,7 +1,8 @@
 from typing import Any, Sequence
 
-def seq_seach(a: Sequence, key: Any) -> int:
-    i =0
+
+def seq_search(a: Sequence, key: Any) -> int:
+    i = 0
 
     while True:
         if i == len(a):
@@ -10,10 +11,18 @@ def seq_seach(a: Sequence, key: Any) -> int:
             return i
         i += 1
 
+
 if __name__ == '__main__':
     num = int(input('Enter the numbers'))
     x = [None] * num
 
     for i in range(num):
         x[i] = int(input(f'x[{i}]: '))
+    ky = int(input('Enter the search value: '))
 
+    idx = seq_search(x, ky)
+
+    if idx == -1:
+        print('Not exist')
+    else:
+        print(f'Search Val x[{idx}].')

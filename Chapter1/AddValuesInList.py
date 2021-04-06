5555df5fa0025b10ec5edb8655f85f8789c8e753
gunny <gunny910303@gmail.com>
2021-04-06 22:13:53 +0900

    Solution for Given List and x element that insert to List

---
+++ Chapter1/AddValuesInList.py
@@ -0,0 +1,14 @@
+def solution(L, x):
+    answer = []
+
+    for i in range(len(L)):
+        if (L[i] > x):
+            L.insert(i, x)
+            break
+        elif L[-1] < x:
+            L.append(x)
+            break
+
+    answer = L
+
+    return answer

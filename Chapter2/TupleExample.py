e0085f548e937d484364192c7783b679a1ecec45
gunny <gunny910303@gmail.com>
2021-04-11 22:00:32 +0900

    Tuple example

--- Chapter2/TupleExample.py
+++ Chapter2/TupleExample.py
@@ -9,3 +9,14 @@ if tuple1 == tuple2 == tuple3:
     tuple1 = (11, 22, 33)
     for i in range(len(tuple1)):
         print(tuple1[i])
+
+
+x = 3
+y = 5
+
+position = x,y
+
+print("x, y로 이루어진 튜플 position의 값은 {}입니다.".format(position))
+
+a, b = 1, 2
+c = a, b      #c = (1, 2)

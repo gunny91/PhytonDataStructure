242fd548fca94407b1bcce0811214434c0218ee1
gunny <gunny91@iastate.edu>
2021-04-26 23:45:58 +0900

    Binary tree depth() update

--- Chapter3/BinarayTree.py
+++ Chapter3/BinarayTree.py
@@ -1,17 +1,3 @@
-'''
-이미 주어진 코드 (class Node 와 class BinaryTree 에 의하여) 의 구조를 따르는
-이진 트리 (binary tree) 에 대하여, 트리의 깊이 (depth) 를 구하는 연산의 구현을 완성하세요.
-초기 코드에 pass 로만 되어 있는 class Node 의 depth() 메서드와 class BinaryTree 의 depth() 메서드를 구현합니다.
-코드의 다른 부분은 수정할 필요가 없습니다.
-
-size() 메서드들 (class Node 와 class BinaryTree 에 대해서) 을 그대로 두었습니다.
-문제로 주어진 depth() 연산도 매우 비슷한 식으로 구현할 수 있으니 참고로 삼으세요.
-
-[참고] 실행 을 눌렀을 때 통과하는 것은 아무 의미 없습니다.
-
-
-'''
-
 class Node:
 
     def __init__(self, item):
@@ -27,7 +13,16 @@ class Node:
 
 
     def depth(self):
-        pass
+        left = self.left.depth() if self.left else 0
+        right = self.right.depth() if self.right else 0
+
+        if left > right :
+            return left + 1
+        #elif right > left:
+        #    return right + 1
+        else: return right + 1
+            #return 0
+
 
 
 class BinaryTree:
@@ -37,13 +32,18 @@ class BinaryTree:
 
     def size(self):
         if self.root:
-            return self.root.size()
+              return self.root.depth()
         else:
-            return 0
+            return None
 
 
     def depth(self):
-        pass
+        if self.root:
+            return self.root.depth()
+        elif self.root ==1:
+            return 1
+        else:
+            return 0
 
 
 def solution(x):
